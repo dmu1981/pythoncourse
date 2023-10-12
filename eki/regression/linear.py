b02b21cac5276a58130cb7ee3dd4573fdefbd68e
Dennis Müller <mhulla1981@googlemail.com>
2023-10-12 12:57:02 +0200

    Use np.stack instead of concatente

--- eki/regression/linear.py
+++ eki/regression/linear.py
@@ -2,15 +2,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Define our points
-coords_x = np.array([[1,2,3]]).T
-coords_y = np.array([[3,4,4]]).T
+coords_x = np.array([1,2,3])
+coords_y = np.array([3,4,4])
 
 # Our linear model is y = w_0 + w_1 * x, setup the X matrix and Y vector
-X = np.concatenate(
-    (np.ones_like(coords_x), coords_x),
-    axis=1
-)
+X = np.stack([
+    np.ones_like(coords_x), coords_x], axis=1)
 Y = coords_y
+print(X)
 
 # Estimate model paramaters using pseudo-inverse
 Xinv = np.linalg.inv(X.T @ X) @ X.T
