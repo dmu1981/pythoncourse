682a00351ff1d52bca2691082bb813477e9e9a56
Dennis <mhulla1981@googlemail.com>
2023-11-14 19:13:53 +0100

    Praktikumsaufgabe 15.11.2023

--- eki/svm/aufgabe.py
+++ eki/svm/aufgabe.py
@@ -28,31 +28,20 @@ X_normalized = scaler.transform(X)
 #
 # # Verwenden Sie einen Polynomial-Kernel Grad 2
 def Aufgabe1():
-  from sklearn import svm
-  svc = svm.SVC(kernel="rbf", gamma=0.1, C=100)
-  svc.fit(X_normalized, Y)
-  return svc
+  return None
+  pass
 
 # Aufgabe 2
 # Erstellen Sie eine Prädiktion für jeden Punkt und zeichnen Sie korrekt klassifizierte 
 # Punkte als grüne Diamanten sowie falsch klassifizierte Punkte als schwarze Kreuze.
 # Hinweis: Grüne Diamante bekommen Sie mit dem "gD" Parameter, Schwarze Kreuze mit dem "kx" Parameter.
 def Aufgabe2(svc):
-  prediction = svc.predict(X_normalized)
-  correctIndices = (prediction == Y)
-
-  plt.plot(X[correctIndices, 0],X[correctIndices, 1], "gD")
-
-  wrongIndices = (prediction != Y)
-  plt.plot(X[wrongIndices, 0],X[wrongIndices, 1], "kx")
+  pass
 
 # Aufgabe 3
 # Wieviel Prozent der Daten werden korrekt klassifiziert?
 def Aufgabe3(svc):
-  prediction = svc.predict(X_normalized)
-  correctIndices = (prediction == Y)
-  accuracy = np.sum(correctIndices) / Y.shape[0]
-  print(f"Accuracy={100.0*accuracy:.3f}%")
+  pass
 
 # Aufgabe 4
 # Experimentieren Sie mit verschiedenen Kerneln und Parametrisierungen um die Genauigkeit zu erhöhen
@@ -65,27 +54,7 @@ def Aufgabe4():
 # Durchsuchen Sie dazu mit np.linspace(0, 2.5, 20) und np.geomspace(0.1, 1000, 20) verschiedenen Werte für GAMMA und C,
 # fitten sie jeweils die SVM und bestimmen Sie die Genauigkeit. Behalten Sie die Parameter, welche die beste Genauigkeit liefern
 def Aufgabe5():
-  from sklearn import svm
-  best_gamma = 0
-  best_C = 0
-  best_accuracy = 0
-  for gamma in np.linspace(1.0, 5.0, 30):
-    for C in np.geomspace(0.1, 1000, 20):
-      svc = svm.SVC(kernel="rbf", gamma=gamma, C=C)
-      svc.fit(X_normalized, Y)
-      prediction = svc.predict(X_normalized)
-      correctIndices = (prediction == Y)
-      accuracy = np.sum(correctIndices) / Y.shape[0]
-      if accuracy > best_accuracy:
-        best_accuracy = accuracy
-        best_gamma = gamma
-        best_C = C
-        
-        print(f"Neuer Bestwert: gamma={gamma:.3f}, C={C:.3f}, accuracy={100*accuracy:.3f}%")
-  
-  best_svc = svm.SVC(kernel="rbf", gamma=best_gamma, C=best_C)
-  best_svc.fit(X_normalized, Y)
-  return best_svc
+  pass
 
 # Aufgabe 6
 # Zeichnen Sie zu ihrer besten SVM aus Aufgabe 5 die Trennfläche als Contour. 
@@ -98,18 +67,7 @@ def Aufgabe5():
 #     plt.contourf(xx, yy, z, levels=[-1,0,1], cmap="seismic", alpha=0.2)
 
 def Aufgabe6(svc):
-  N = 150
-  x_range = np.linspace(-8,8,N)
-  y_range = np.linspace(-8,8,N)
-
-  z = np.zeros((N, N))
-  for x_index, x  in enumerate(x_range):
-    for y_index, y in enumerate(y_range):
-      coord = scaler.transform(np.array([[x,y]]))
-      z[y_index, x_index] = svc.predict(coord)
-
-  xx, yy = np.meshgrid(x_range, y_range)
-  plt.contourf(xx, yy, z, levels=[-1,0,1], cmap="seismic", alpha=0.2)
+  pass
 
 
 
