ef1e47a6a4814ea616fe50eb1aaf907cdff9b1d1
Dennis <mhulla1981@googlemail.com>
2023-11-28 19:20:40 +0100

    Zweite Praktikumsaufgabe, 29.11.2023

--- eki/perzeptron/aufgabe2.py
+++ eki/perzeptron/aufgabe2.py
@@ -9,9 +9,7 @@ from matplotlib import pyplot as plt
 #
 #   https://scikit-learn.org/stable/datasets/toy_dataset.html#digits-dataset
 #
-data = datasets.load_digits()
-X = data["data"]
-Y = data["target"]
+
 
 # Aufgabe 2
 # Erzeugen Sie mit 
@@ -29,22 +27,14 @@ Y = data["target"]
 #
 # um dieses Bild anzuzeigen. Wählen Sie eine geeignete Colormap über die cmap Option
 # und setzen sie den Titel des Plots auf die jeweilige Klasse 
-fig, axs = plt.subplots(5,8)
-for row in range(5):
-  for col in range(8):
-    index = int(np.floor(np.random.uniform(0.0, X.shape[0]-1)))
 
-    axs[row,col].imshow(X[index,:].reshape(8,8), cmap="gray")
-    axs[row,col].set_title(Y[index])
-plt.show()
 
 # Aufgabe 3
 # Unser Perzeptron soll die Klasse 4 von allen anderen Klassen unterscheiden
 # Finden sie dazu alle Indices der positiven (4) und negativen (nicht 4) Klasse.
 # Invertieren Sie wie im Skript die Datenpunkte der negativen Klasse (vgl. vorherige Aufgabe)
-positive_indices = (Y == 4)
-negative_indices = (Y != 4)
-X[negative_indices] *= -1
+
+
 
 # Aufgabe 4
 # Wählen sie mit np.random.normal(...) einen normalverteilten Startvektor w der richten Größe
@@ -55,16 +45,4 @@ X[negative_indices] *= -1
 # so lange zu warten bis alle Punkte korrekt klassifiziert werden. 
 # Bestimmen sie nach jedem Update die Genauigkeit (Accuracy) ihres Perzeptrons und geben Sie diese auf der
 # Konsole aus. 
-w = np.random.normal(0.0, 1.0, 64)
-
-for step in range(1000):
-  index = int(np.floor(np.random.uniform(0.0, X.shape[0]-1)))
-  if X[index, :] @ w > 0:
-    continue
-  
-  w = w + X[index,:]
-
-  correct = np.sum(X @ w > 0)
-  acc = correct / X.shape[0]
-  print(f"Accuracy: {100.0*acc:.2f}%")
 
